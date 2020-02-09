1fb2b4a2480cfab5da01b00198211c7b06fbb655
KojoBoateng <egyirchristian07@gmail.com>
2020-02-09 23:08:08 +0000

    Made the dashboard for the lecturers

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,30 +3,31 @@
 @section('body')
 
 
-<div class="table-responsive">
 
-    <table class="table table-striped">
-        <thead>
-        <tr>
-            <th>#id</th>
-            <th>Image</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Type</th>
-            <th>Price</th>
-            <th>Edit Image</th>
-            <th>Edit</th>
-            <th>Remove</th>
-        </tr>
-        </thead>
-        <tbody>
+    <div class="table-responsive">
 
-   
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>#id</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Type</th>
+                <th>Price</th>
+                <th>Edit Image</th>
+                <th>Edit</th>
+                <th>Remove</th>
+            </tr>
+            </thead>
+            <tbody>
 
-        </tbody>
-    </table>
+       
 
-   
+            </tbody>
+        </table>
 
-</div>
+       
+
+    </div>
 @endsection
