efda9d5e2ce085f0d87457da11d4c30f7462991e
KojoBoateng <egyirchristian07@gmail.com>
2020-02-26 14:52:32 +0000

    The students model

---
+++ app/Student.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    public $timestamps = false;
+
+
+    // Specifying the columns to be filled in the students table
+    protected $fillable =['name','index'];
+
+}
