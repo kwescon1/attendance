fd121385ea81603c0c75681f93c84c5b163e3273
KojoBoateng <egyirchristian07@gmail.com>
2020-02-26 14:51:23 +0000

    Created the api routes for the app side of this project

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Get the number of students in the table
+
+Route::get('/students', ['uses' => 'StudentsController@index']);
+
+
+Route::post('/registerstudent',['uses' => 'StudentsController@studentRegister']);																																														
